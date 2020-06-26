1fa240d3298da0446a1a07b3b7253bf967266af0
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-06-26 13:05:20 +0300

    add structure

---
+++ src/components/constants/routes.js
@@ -0,0 +1,6 @@
+export const HOME = '/';
+export const EVENTS = '/events';
+export const EVENT_DETAIL = '/event/:id';
+export const SIGNIN = '/signin';
+export const SIGNUP = '/signup';
+export const ADD_EVENT = '/event';
