31c87df306daae02d4d9d40be27b1036eac4aab9
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-07-09 13:14:51 +0300

    переделал таблицу

--- src/components/events/EventsList.js
+++ src/components/events/EventsList.js
@@ -1,56 +1,176 @@
-import React, { useState } from 'react'
-import EventSummary from "./EventSummary";
-import Button from '../Button'
-import ButtonChange from '../Filter/ButtonChange';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles, useTheme } from '@material-ui/core';
+import { FirstPage, KeyboardArrowLeft, KeyboardArrowRight, LastPage, Delete } from "@material-ui/icons";
+import {
+  Table, TableBody, TableCell, TableContainer, TableFooter, TablePagination, TableRow, Paper, IconButton, TableHead
+} from "@material-ui/core";
 
-const INITIAL_STATE = []
-const showRows = 6;
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
-export default function EventsList({ events = INITIAL_STATE }) {
+import moment from 'moment';
+import { removeEvent } from '../../store/actions/eventActions';
 
-  const [currentEvents, setCurrentEvents] = useState(0)
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
 
-  if (!events.length) return <div>No data</div>;
+function TablePaginationActions(props) {
+  const classes = useStyles1();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
 
-  const visibleEvents = events.slice(currentEvents, currentEvents + showRows);
-  const totalPages = Math.ceil((events.length) / 6)
-  let arrNumbers = []
-  for (let i = 1; i <= totalPages; i++) {
-    arrNumbers.push(i)
-  }
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
 
-  const prevEvents = () => setCurrentEvents(currentEvents - showRows)
-  const nextEvents = () => setCurrentEvents(currentEvents + showRows)
-  const disabled = (right) => right ? currentEvents >= events.length - showRows : currentEvents === 0
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
 
-  const headers = ['Date', 'Sum', 'Action']
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
 
   return (
-    <table className="table">
-      <thead className="thead">
-        <tr className="table__row">
-          {headers.map(elem => <th key={elem} className="table__item">{elem}</th>)}
-        </tr>
-      </thead>
-      <tbody className="tbody">
-        {events && visibleEvents.map(event => <EventSummary event={event} key={event.id} />)}
-      </tbody>
-      <tfoot className='tfoot'>
-        <tr className="table__row">
-          <td className='table__item'>
-            <ButtonChange dark onHandler={prevEvents} disabled={disabled()} />
-          </td>
-          <td className='table__item'>
-            {arrNumbers.map((el, i) =>
-              <Button dark onHandler={() =>
-                setCurrentEvents(i * showRows)} disabled={currentEvents === i * showRows} key={el}>{el}
-              </Button>)}
-          </td>
-          <td className='table__item'>
-            <ButtonChange dark onHandler={nextEvents} disabled={disabled('right')} rotate />
-          </td>
-        </tr>
-      </tfoot>
-    </table>
-  )
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPage /> : <FirstPage />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPage /> : <LastPage />}
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
+
+const useStyles2 = makeStyles({
+  table: {
+    minWidth: 300,
+  },
+});
+
+const INITIAL_STATE = [];
+
+export default function CustomPaginationActionsTable({ events = INITIAL_STATE }) {
+  const classes = useStyles2();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const emptyRows = rowsPerPage - Math.min(rowsPerPage, events.length - page * rowsPerPage);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const removeItem = (e, id) => {
+    e.stopPropagation();
+    dispatch(removeEvent(id))
+  }
+  const translateTo = (e, id) => {
+    e.stopPropagation();
+    history.push(`event/${id}`)
+  }
+
+
+  const columns = [
+    { name: 'date', title: 'Дата' },
+    { name: 'sum', title: 'Сумма' },
+    { name: 'action', title: 'Действия' }
+  ]
+
+  const className = method => method === 'outcome' ? 'color-wrapper outcome' : 'color-wrapper income';
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="custom pagination table">
+        <TableHead>
+          <TableRow>
+            {columns.map(col => <TableCell key={col.name}>{col.title}</TableCell>)}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0 ? events.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : events)
+            .map((row) =>
+              <TableRow key={row.id} onClick={e => translateTo(e, row.id)} style={{ cursor: 'pointer' }}>
+                <TableCell>
+                  {row.tag} <br />
+                  {moment(row.date.toDate()).format('DD.MM.YY')}
+                </TableCell>
+                <TableCell>
+                  <span className={className(row.method)}>{row.sum}</span>
+
+                </TableCell>
+                <TableCell>
+                  <Delete style={{ cursor: 'pointer' }} onClick={e => removeItem(e, row.id)} />
+                </TableCell>
+              </TableRow>
+            )}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={events.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: { 'aria-label': 'rows per page' },
+                native: true,
+              }}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
 }
